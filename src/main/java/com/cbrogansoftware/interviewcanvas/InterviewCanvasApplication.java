fb22d8d2a5dee134291fe915ef93484c9cbd1ed9
cbrogansoftware <cbrogansoftware@gmail.com>
2022-10-17 09:24:28 -0500

    Project to use during interviews.  Has just the following: Spring Boot and Main entry points, Spring Boot and main tests, and a Printer class as a placeholder.

---
+++ src/main/java/com/cbrogansoftware/interviewcanvas/InterviewCanvasApplication.java
@@ -0,0 +1,13 @@
+package com.cbrogansoftware.interviewcanvas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewCanvasApplication {
+
+    public static void main(String[] args) {SpringApplication.run(InterviewCanvasApplication.class, args);}
+
+
+
+}
---
+++ src/test/java/com/cbrogansoftware/interviewcanvas/InterviewCanvasApplicationTests.java
@@ -0,0 +1,15 @@
+package com.cbrogansoftware.interviewcanvas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class InterviewCanvasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
